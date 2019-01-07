1969b19f3221c09d12767a1d5cc697d3ee7a4584
Weihan Lin <weihanlin@live.com>
2019-01-06 21:58:10 -0500

    Correct previous mistake to add multi-folder support

--- dl-baidu.sh
+++ dl-baidu.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+	read -p "Name of the folder to download:" dirname
+  else
+	dirname="$@"
+fi
 
 BaiduPCS-Go config set -appid=266719
 
 sleep 1
 
 BaiduPCS-Go cd /
-
-
-if [ $# -eq 0 ]
-  then
-    read -p "Name of the folder to download:" dirname
-  else dirname="$1"
-fi
 
 BaiduPCS-Go mv $dirname /apps/baidu_shurufa/
 
@@ -24,4 +23,4 @@ sleep 1
 
 tmux new-session -d -s `date +%H-%M-%S` BaiduPCS-Go d $dirname
 
-exit 0
+
